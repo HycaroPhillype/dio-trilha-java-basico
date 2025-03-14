9d5bd325c9e4e8d07147450cdb175bd10f9981e6
Hycaro Phillype <phillypehycaro@gmail.com>
2025-03-14 11:03:36 -0300

    feat: ContaBanco finalizada com sucesso

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,35 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+         System.out.print("Por favor, digite o número da Conta: ");
+         int numero = scanner.nextInt();
+         scanner.nextLine(); 
+ 
+         System.out.print("Por favor, digite o número da Agência: ");
+         String agencia = scanner.nextLine();
+ 
+         System.out.print("Por favor, digite o Nome do Cliente: ");
+         String nomeCliente = scanner.nextLine();
+ 
+         System.out.print("Por favor, digite o Saldo: ");
+         double saldo = scanner.nextDouble();
+ 
+         String mensagem = "Olá ".concat(nomeCliente)
+                 .concat(", obrigado por criar uma conta em nosso banco, sua agência é ")
+                 .concat(agencia)
+                 .concat(", conta ")
+                 .concat(String.valueOf(numero))
+                 .concat(" e seu saldo ")
+                 .concat(String.valueOf(saldo))
+                 .concat(" já está disponível para saque.");
+ 
+         System.out.println(mensagem);
+ 
+         scanner.close();
+
     }
 }
